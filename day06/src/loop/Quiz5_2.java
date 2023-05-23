2865b69e9ed13ff4d07c94a4d6156ee0c3e8a646
LeeBG <donny1848@gmail.com>
2023-05-23 15:39:26 +0900

    스택을 활용한 기법 +

--- day06/src/loop/Quiz5_2.java
+++ day06/src/loop/Quiz5_2.java
@@ -3,42 +3,37 @@ package loop;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Quiz5 {
+public class Quiz5_2 {
 	public static void main(String[] args) {
 		/* 
 		 * 천단위 구분 기호 (,)직접 구현하기
 		 * 
 		 */
-		
 		Scanner sc = new Scanner(System.in);
 		int n1;
-		String n2 = "";
-		String tmp = "";
-		String result = "";
+		String n2="";
 		int count = 0;
+		Stack<String> tmpStack = new Stack<>();
 		
 		System.out.print("입력 : ");
 		n1 = sc.nextInt();
 		
 		// ,로 분기시키면서 뒤집기
 		while(n1 != 0) {
-			tmp += n1 % 10;
+			tmpStack.push(n1 % 10+"");
 			n1 /= 10;
 			count ++;
-			if(count %3 == 0 && n1 > 0) {
-				tmp += ",";
+			if(count % 3 == 0 && n1 > 0) {
+				tmpStack.push(",");
 			}
 		}
+		System.out.println("tmp : "+tmpStack.toString());
 		
-		System.out.println("tmp : " + tmp );
+		int stacknum = tmpStack.size();
 		
-		// 다시 뒤집기
-		for(int i=tmp.length()-1;i>=0;i--) {
-			n2 += tmp.charAt(i); 
+		for(int i=0;i<stacknum;i++) {
+			n2+=tmpStack.pop();
 		}
-		
-		System.out.println("n2 : "+ n2);
-		
-		sc.close();
+		System.out.println("n2 : " + n2);
 	}
 }
